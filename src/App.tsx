a1a1131ba644c5a999187a21b6950a214c12c4b1
E01T <etsouderos@gmail.com>
2021-08-22 09:59:03 +0300

    Github user search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,483 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent } from 'react'
+import './App.css'
+import { calcNoOfPages, isSequence, createNewPagesArray } from './utils'
+import { Search, ChevronLeft, ChevronRight, Info } from 'react-bootstrap-icons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type users_type = {
+  total_count: number
+  items: Array<user_type>
 }
 
-export default App;
+type user_type = {
+  avatar_url: string | undefined
+  bio: string | null
+  blog: string
+  company: string | null
+  created_at: string
+  email: string | null
+  events_url: string | null
+  followers: number
+  followers_url: string | null
+  following: 0
+  following_url: string | null
+  gists_url: string | null
+  gravatar_id: string
+  hireable: boolean | null
+  html_url: string
+  id: number
+  location: string | null
+  login: string
+  name: string | null
+  node_id: string
+  organizations_url: string | null
+  public_gists: number
+  public_repos: number
+  received_events_url: string | null
+  repos_url: string | null
+  site_admin: boolean
+  starred_url: string | null
+  subscriptions_url: string | null
+  twitter_username: string | null
+  type: string
+  updated_at: string | null
+  url: string
+}
+
+const getUser = (user: string) =>
+  fetch(`https://api.github.com/users/${user}`, {
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+    },
+  }).then((response) => response.json())
+
+const searchGithubApi = (userName: string, pageNo: number = 1) =>
+  fetch(
+    `https://api.github.com/search/users?q=${userName}&type=users&per_page=10&page=${pageNo}`,
+    {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+      },
+    }
+  ).then((res) => res.json())
+
+function PageView({
+  users,
+  children,
+}: {
+  users: user_type[]
+  children: React.ReactNode
+}) {
+  console.log('PageView')
+  const usersProfiles = users.map((user: user_type) => {
+    return (
+      <div className="card" style={{ maxWidth: '600px' }} key={user.id}>
+        <div className="row">
+          <div className="col-md-4 ">
+            <img
+              className="img-fluid rounded-start"
+              style={{ width: '80px' }}
+              src={user.avatar_url}
+              alt="user avatar"
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href={user.html_url} rel="noreferrer" target="_blank">
+                  {user.login}
+                </a>
+                &nbsp;&nbsp;&nbsp;
+                <a href={user.html_url} rel="noreferrer" target="_blank">
+                  {user.name}
+                </a>
+              </h5>
+              <p className="card-text">{user.bio}</p>
+              <p className="card-text">
+                <span>{user.followers} followers</span>{' '}
+                <span>Following: {user.following}</span>
+              </p>
+              {user.location ? (
+                <p className="card-text"> Location: {user.location}</p>
+              ) : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  })
+  return (
+    <>
+      {usersProfiles}
+      {children}
+    </>
+  )
+}
+
+function arraysIdentical(
+  prevProps: Readonly<{ users: user_type[]; children: React.ReactNode }>,
+  currentProps: Readonly<{ users: user_type[]; children: React.ReactNode }>
+) {
+  const a = prevProps.users
+  const b = currentProps.users
+
+  let i = a.length
+  if (i !== b.length) return false
+  while (i--) {
+    if (a[i] !== b[i]) return false
+  }
+  return true
+}
+
+const MemoizedPageView = React.memo(PageView, arraysIdentical)
+
+function PreviousPage({
+  pageNo,
+  setPageNo,
+}: {
+  pageNo: number
+  setPageNo: (pageNo: number) => void
+}) {
+  const [disabled, setDisabled] = React.useState(false)
+
+  React.useEffect(() => {
+    if (pageNo === 1) setDisabled(true)
+    else setDisabled(false)
+  }, [pageNo])
+
+  return (
+    <button
+      id="prev-btn"
+      className="btn btn-outline-primary btn-sm"
+      disabled={disabled}
+      onClick={() => setPageNo(pageNo - 1)}
+      style={{ borderColor: 'transparent' }}
+    >
+      <ChevronLeft className="me-2 mb-0" />
+      Previous
+    </button>
+  )
+}
+
+function NextPage({
+  pageNo,
+  setPageNo,
+  last_page,
+}: {
+  pageNo: number
+  setPageNo: (pageNo: number) => void
+  last_page: number
+}) {
+  const [disabled, setDisabled] = React.useState(false)
+
+  React.useEffect(() => {
+    if (pageNo === last_page) setDisabled(true)
+    else setDisabled(false)
+  }, [pageNo, last_page])
+
+  return (
+    <button
+      id="next-btn"
+      className="btn btn-outline-primary btn-sm"
+      disabled={disabled}
+      onClick={() => setPageNo(pageNo + 1)}
+      style={{ borderColor: 'transparent' }}
+    >
+      Next
+      <ChevronRight className="ms-2 mb-0" />
+    </button>
+  )
+}
+
+function PageNumberInput({
+  setPageNo,
+}: {
+  setPageNo: (pageNo: number) => void
+}) {
+  const [userInput, setUserInput] = React.useState('')
+
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value.trim()
+    setUserInput(value)
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setPageNo(parseInt(userInput))
+    setUserInput('')
+  }
+
+  return (
+    <form
+      id="page-number-input"
+      className="d-flex justify-content-center"
+      onSubmit={handleSubmit}
+    >
+      <label
+        style={{ display: 'none' }}
+        className="form-label"
+        htmlFor="page-no"
+      >
+        Page No:
+      </label>
+      <input
+        id="page-no"
+        autoComplete="off"
+        type="text"
+        maxLength={2}
+        className="form-control form-control-inline"
+        placeholder="#"
+        value={userInput}
+        onChange={handleChange}
+      />
+      <button
+        style={{ display: 'inline' }}
+        className="btn btn-outline-primary"
+        type="submit"
+      >
+        Go
+      </button>
+    </form>
+  )
+}
+
+function createPagesList(
+  newPagesArray: number[],
+  pageNo: number,
+  setPageNo: (pageNo: number) => void
+) {
+  const pages = []
+  for (let i = 0; i < newPagesArray.length; i++) {
+    pages.push(
+      <li
+        key={i}
+        className="page-item"
+        style={{ display: 'inline', margin: '.3em .6em' }}
+      >
+        {pageNo === newPagesArray[i] ? ( // if this is the current page apply special style
+          <button
+            className="btn btn-outline-secondary btn-sm pagination-btn"
+            style={{
+              borderColor: 'transparent',
+              padding: '10px',
+              borderRadius: '50%',
+              background: 'rgb(3, 102, 214)',
+              color: 'white',
+              boxShadow:
+                '10px 10px 10px rgb(0 0 0 / 40%), inset -5px -5px 10px rgb(0 0 0 / 40%)',
+            }}
+            onClick={() => setPageNo(newPagesArray[i])}
+          >
+            {newPagesArray[i]}
+          </button>
+        ) : newPagesArray[i] === -1 ? (
+          <span className="gap">…</span>
+        ) : (
+          <button
+            className="btn btn-outline-secondary btn-sm"
+            style={{
+              borderColor: 'transparent',
+              padding: '10px',
+              borderRadius: '50%',
+              boxShadow:
+                '10px 10px 10px rgb(0 0 0 / 40%), inset -5px -5px 10px rgb(0 0 0 / 40%)',
+            }}
+            onClick={() => setPageNo(newPagesArray[i])}
+          >
+            {newPagesArray[i]}
+          </button>
+        )}
+      </li>
+    )
+  }
+  return pages
+}
+
+const Pagination = ({
+  total_count,
+  pageNo,
+  setPageNo,
+}: {
+  total_count: number
+  pageNo: number
+  setPageNo: (pageNo: number) => void
+}) => {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
+
+  // Rezing the window event and set window width
+  React.useEffect(() => {
+    window.addEventListener('resize', (e: Event) => {
+      setWindowWidth(window.innerWidth)
+    })
+  }, [windowWidth])
+
+  const noOfPages = calcNoOfPages(total_count, pageNo, windowWidth)
+  console.log('noOfPages', noOfPages)
+  const indexes = isSequence(noOfPages)
+  // console.log('indexes', indexes)
+  const newPagesArray = createNewPagesArray(noOfPages, indexes)
+  // console.log('newPagesArray', newPagesArray)
+  const last_page = noOfPages[noOfPages.length - 1]
+  // console.log('last_page', last_page)
+
+  const pages = createPagesList(newPagesArray, pageNo, setPageNo)
+
+  return pages.length > 0 ? (
+    <>
+      <div className="d-flex mt-1 justify-content-center text-primary">
+        <output id="pages-output" style={{ fontSize: '.8em' }}>
+          <Info />
+          Pages: {pageNo}/{newPagesArray[newPagesArray.length - 1]}
+        </output>
+      </div>
+      <div id="pages" style={{ maxWidth: '100%' }}>
+        <div id="previous-page-comp">
+          <PreviousPage pageNo={pageNo} setPageNo={setPageNo} />
+        </div>
+        {windowWidth > 589 ? (
+          <div id="pages-ul">
+            <ul style={{ display: 'inline', padding: '0 5px' }}>{pages}</ul>
+          </div>
+        ) : (
+          <PageNumberInput setPageNo={setPageNo} />
+        )}
+
+        <div id="next-page-comp">
+          <NextPage
+            pageNo={pageNo}
+            setPageNo={setPageNo}
+            last_page={last_page}
+          />
+        </div>
+      </div>
+    </>
+  ) : null
+}
+
+const MemoizedPagination = React.memo(Pagination)
+
+function App() {
+  const [users, setUsers] = React.useState<users_type>({
+    total_count: 0,
+    items: [],
+  })
+
+  const [inputValue, setInputValue] = React.useState<string>('') // handle change of input field
+
+  const [userInput, setUserInput] = React.useState<string>('')
+  const [pageNo, setPageNo] = React.useState<number>(1)
+  // const [state, setState] = React.useState<users_type2>({total_count: 0, pageNo: 0, items: []});
+
+  const [outputMessage, setOutputMessage] = React.useState<string>('')
+
+  const topRef = React.useRef<HTMLDivElement | null>(null)
+
+  React.useEffect(() => {
+    // console.log('userInput', userInput, 'pageNo', pageNo)
+    if (userInput === '') return // exit if userInput is empty (initial state)
+
+    searchGithubApi(userInput, pageNo).then(
+      (users) => {
+        const total_count: number = users.total_count
+
+        if (total_count === 0) {
+          setOutputMessage('No users found')
+          return // exit
+        }
+
+        const userPromises = users.items.map((user: user_type) => {
+          return getUser(user.login)
+        })
+
+        Promise.all<user_type>(userPromises).then((users) => {
+          setUsers({ items: users, total_count: total_count })
+          topRef?.current?.scrollIntoView({ behavior: 'smooth' })
+        })
+      },
+      (err) => console.error(err)
+    )
+  }, [userInput, pageNo])
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // reset all state
+    setUsers({ items: [], total_count: 0 })
+    setPageNo(1)
+    setInputValue('')
+    setOutputMessage('')
+    setUserInput(inputValue)
+  }
+
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value
+    setInputValue(value)
+  }
+  console.log('users.items', users.items)
+  return (
+    <div ref={topRef} className="container-lg" style={{ margin: '0px auto' }}>
+      <div className="container-lg">
+        <form
+          className="d-flex justify-content-center mt-2"
+          onSubmit={handleSubmit}
+        >
+          <div className="col-auto">
+            <label
+              className="form-label visually-hidden"
+              style={{ marginRight: '.3em' }}
+              htmlFor="search"
+            >
+              Find users
+            </label>
+          </div>
+          <div className="col-auto">
+            <div className="input-group mb-3">
+              <span className="input-group-text">
+                <Search />
+              </span>
+              <input
+                id="search"
+                className="form-control"
+                type="text"
+                value={inputValue}
+                onChange={handleChange}
+                placeholder="Search for users"
+              />
+            </div>
+          </div>
+          <div className="col-auto">
+            <button className="btn btn-outline-secondary" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="container-lg">
+        <div className="row">
+          <div className="d-flex justify-content-center text-danger">
+            <output>{outputMessage}</output>
+          </div>
+        </div>
+      </div>
+
+      {users.items.length > 0 ? (
+        <div className="container-lg">
+          <div className="row d-flex justify-content-center">
+            <div className="col-auto">
+              <h2>Total users: {users.total_count}</h2>
+            </div>
+          </div>
+          <div className="row d-flex justify-content-center">
+            <MemoizedPageView users={users.items}>
+              <MemoizedPagination
+                total_count={users.total_count}
+                pageNo={pageNo}
+                setPageNo={setPageNo}
+              />
+            </MemoizedPageView>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  )
+}
+
+export default App
---
+++ src/utils.ts
@@ -0,0 +1,205 @@
+// get an array of numbers of a specific range form - to inclusive
+function range(from: number, to: number): number[] | [] {
+  const range_array = []
+  for (let i = from; i <= to; i++) {
+    range_array.push(i)
+  }
+  return range_array
+}
+
+/**
+ * This is an efford to emulate the pagination logic of github search results.
+ * For large screens
+ * Pagination visual analysis -> Use Cases:
+ * Start (first loaded page: < Previous btn inactive, 7 number elements and the (gap) ... 8 total)
+ * < Previous 1 2 3 4 5 ... 99 100 Next >
+ * Pressing btn 2 (same as above)
+ * < Previous 1 2 3 4 5 ... 99 100 Next >
+ * Pressing btn 3 (same as above)
+ * < Previous 1 2 3 4 5 ... 99 100 Next >
+ * Pressing btn 4 (number 6 btn added, 8 number elements and the ... 9 total)
+ * < Previous 1 2 3 4 5 6 ... 99 100 Next > 
+ * Pressing btn 5 (number 7 btn added, 9 number elements and the ... 10 total)
+ * < Previous 1 2 3 4 5 6 7 ... 99 100 Next >
+ * Pressing btn 6 (number 8 btn added, 10 number elements and the ... 11 total)
+ * < Previous 1 2 3 4 5 6 7 8 ... 99 100 Next >
+ * Pressing btn 7 (number 3 and 4 removed and ... added in place, btn 9 added, 9 number elements and the 2 ... elements 11 total)
+ * < Previous 1 2 ... 5 6 7 8 9 ... 99 100 Next >
+ * Pressing btn 8 (number 5 removed and ... added in place, btn 10 added, 9 number elements and the 2 ... elements 11 total)
+ * < Previous 1 2 ... 6 7 8 9 10 ... 99 100 Next >
+ * Pressing btn 9 (number 6 removed and ... added in place, btn 11 added, 9 number elements and the 2 ... elements 11 total)
+ * < Previous 1 2 ... 7 8 9 10 11 ... 99 100 Next >
+ * Pressing btn 10 (number 7 removed and ... added in place, btn 12 added, 9 number elements and the 2 ... elements 11 total)
+ * < Previous 1 2 ... 8 9 10 11 12 ... 99 100 Next >
+ * //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ * END of paggination
+ * Pressing btn 90 (Start)
+ * < Previous 1 2 ... 88 89 90 91 92 ... 99 100 Next >
+ * Pressing btn 91 (number 88 removed, btn 93 added, 9 number elements and the 2 ... elements 11 total)
+ * < Previous 1 2 ... 89 90 91 92 93 ... 99 100 Next >
+ * Pressing btn 92 (number 89 removed, btn 94 added, 9 number elements and the 2 ... elements 11 total)
+ * < Previous 1 2 ... 90 91 92 93 94 ... 99 100 Next >
+ * Pressing btn 93 (number 90 removed, btn 95 added, 9 number elements and the 2 ... elements 11 total)
+ * < Previous 1 2 ... 91 92 93 94 95 ... 99 100 Next >
+ * Pressing btn 94 (number 91 removed, btn 96 added, 9 number elements and the 2 ... elements 11 total)
+ * < Previous 1 2 ... 92 93 94 95 96 ... 99 100 Next >
+ * Pressing btn 95 (number 92 removed and the last gap (the one before the 99, 100), 
+ * number 97 and 98 btns added, 10 number elements and the ... 11 total)
+ * < Previous 1 2 ... 93 94 95 96 97 98 99 100 Next >
+ * Pressing btn 96 (number 93 removed, 9 number elements and the ... 10 total)
+ * < Previous 1 2 ... 94 95 96 97 98 99 100 Next >
+ * Pressing btn 97 (number 94 removed, 8 number elements and the ... 9 total)
+ * < Previous 1 2 ... 95 96 97 98 99 100 Next >
+ * Pressing btn 98 (number 95 removed, 7 number elements and the ... 8 total)
+ * < Previous 1 2 ... 96 97 98 99 100 Next >
+ * Pressing btn 99 (same as above)
+ * < Previous 1 2 ... 96 97 98 99 100 Next >
+ * Pressing btn 100 (same as above, Next btn inactive) // end of list 
+ * < Previous 1 2 ... 96 97 98 99 100 Next >
+ * Pagination logic for large screens: 
+ * The minimum number of elements are 8 and the maximum are 11 which is the usuall.
+ * Condition 1) I always have the first two (1, 2) and the last two (99, 100) digits no matter what.
+ * Condition 2) I need to have at least 5 digits grouped together, and one or two 2-digit groups(1st and last).
+ * Condition 3) I always need two digits before and two digits after the currently selected number(page).
+                This is not true in the cases where the 1st or the 2nd and the 1 before last and last elements are selected.
+ * Pagination logic for medium screens (795 - 590): 
+ * The minimum number of elements are 8 and the maximum are 11 which is the usuall.
+ * Condition 1) I always have the first (1) and the last (100) digits no matter what.
+ * Condition 2) I need to have at least 3 digits grouped together, and one or two 2-digit groups(1st and last).
+ * Condition 3) I always need 1 digits before and 1 digits after the currently selected number(page).
+                This is not true in the cases where the 1st or the 2nd and the 1 before last and last elements are selected.
+ * Pagination logic for changes to an input field - but we still need the functions below for other calculations in <Pagination />.
+*/
+
+// This function will return and array of numbers that will represent the page numbers or empty array
+function calcNoOfPages(
+  total_users: number,
+  current_page: number,
+  window_width: number
+): number[] | [] {
+  let num_of_pages = 0
+
+  if (total_users >= 1000) {
+    num_of_pages = 100 // 100 is the maximum
+  } else {
+    num_of_pages = Math.ceil(total_users / 10) // e.g 853 = 86 pages // 10 users 10 / 10 = 1 // 8 users 8 / 10 = 1 page // one page is the minimum
+  }
+
+  console.log('num_of_pages start', num_of_pages)
+  // if window width is less than 570px we hide the ul element and we show the pagination buttons and input field
+
+  // TODO: DRY
+  // if window is between 794 and 570px
+  if (window_width >= 590 && window_width < 795) {
+    if (num_of_pages === 1) {
+      return [] /* empty array -. nothing will be rendered (null) since the condition is the array to have length > 0 */
+    } else if (num_of_pages <= 10) {
+      return range(1, num_of_pages)
+    } else {
+      const first_page = 1
+      const last_page = num_of_pages
+
+      if (current_page <= 3) {
+        return [first_page, ...range(2, 5), last_page]
+      }
+
+      if (current_page > num_of_pages - 3) {
+        return [first_page, ...range(96, 99), last_page]
+      }
+
+      // Covering all other cases
+      if (current_page > 3) {
+        return Array.from(
+          new Set([
+            first_page,
+            ...range(current_page - 1, current_page + 1),
+            last_page,
+          ])
+        )
+      }
+    }
+  }
+
+  // if num of pages === 1 do not show any page numbers,
+  if (num_of_pages === 1) {
+    return [] /* empty array -. nothing will be rendered (null) since the condition is the array to have length > 0 */
+  } else if (num_of_pages <= 10) {
+    return range(1, num_of_pages)
+  } else {
+    // get pagesNum and get the first two and the last two numbers
+    const first_pages = [1, 2]
+    const last_pages = [num_of_pages - 1, num_of_pages]
+    // calculate the rest of the display elements
+    // Covering use cases: btn press 1, 2, 3
+    if (current_page <= 3) {
+      return [...first_pages, ...range(3, 5), ...last_pages]
+    }
+    // Covering use cases: btn press 98, 99, 100
+    if (current_page > num_of_pages - 3) {
+      return [...first_pages, ...range(96, 98), ...last_pages]
+    }
+
+    // Covering use cases: btn press 4, 5, 6
+    if (current_page <= 6) {
+      return Array.from(
+        new Set([...first_pages, ...range(3, current_page + 2), ...last_pages])
+      )
+    }
+
+    // Covering use cases: btn press 95, 96, 97
+    if (current_page > num_of_pages - 6) {
+      return Array.from(
+        new Set([...first_pages, ...range(current_page - 2, 98), ...last_pages])
+      )
+    }
+
+    // Covering all other cases
+    if (current_page > 6) {
+      return Array.from(
+        new Set([
+          ...first_pages,
+          ...range(current_page - 2, current_page + 2),
+          ...last_pages,
+        ])
+      )
+    }
+
+    return [] // to satisfy typescript
+  }
+}
+
+/**
+ * isSequence returns an empty array if the array numbers are in a sequence
+ * and an array of index(es) if the array numbers are not in a sequence
+ * it will contain either one or two maximum index(es)
+ * @param {Array} array_of_numbers
+ */
+function isSequence(sequence_array: number[]): number[] | [] {
+  const index_array = []
+  const prevValue = sequence_array[0]
+  for (let i = 0; i < sequence_array.length - 1; i++) {
+    if (prevValue + sequence_array[i] === sequence_array[i + 1]) {
+      continue
+    } else index_array.push(i)
+  }
+  return index_array
+}
+
+function createNewPagesArray(
+  no_of_pages_array: number[] | [],
+  indexes_array: number[] | []
+): number[] | [] {
+  // I put an arbitrary value (-1) in the array as a flag to indicate
+  // that a gap (...) should be added instead of the number.
+  const [first_value, second_value] = indexes_array
+  const result_array = []
+  for (let i = 0; i < no_of_pages_array.length; i++) {
+    if (i === first_value || i === second_value) {
+      result_array.push(no_of_pages_array[i])
+      result_array.push(-1)
+    } else result_array.push(no_of_pages_array[i])
+  }
+  return result_array
+}
+
+export { calcNoOfPages, isSequence, createNewPagesArray }
